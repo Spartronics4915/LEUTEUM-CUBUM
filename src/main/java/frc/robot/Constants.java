07c179d45a13fa46d5aedb3e2721cc30042329f3
Hallam <hallambaesman@gmail.com>
2026-04-03 17:50:18 -0700

    Copy and slightly modify subsystem code from wrist

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,6 +4,11 @@
 
 package frc.robot;
 
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
 /**
  * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
  * constants. This class should not be used for any other purpose. All constants should be declared
@@ -16,4 +21,29 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class ArmRotatorConstants {
+    public static final int MOTOR_ID = 1;
+
+    public static final double P = 0.05;
+    public static final double I = 0;
+    public static final double D = 0;
+
+
+    public static final double MAX_VELOCITY = 5;
+    public static final double MAX_ACCELERATION = 2;
+
+    public static final Rotation2d MIN_ANGLE = Rotation2d.fromDegrees(0);
+    public static final Rotation2d MAX_ANGLE = Rotation2d.fromDegrees(180);
+
+    public static SparkMaxConfig MOTOR_CONFIG = (SparkMaxConfig) new SparkMaxConfig()
+      .inverted(false)
+      .idleMode(IdleMode.kBrake)
+      .smartCurrentLimit(20)
+      .secondaryCurrentLimit(15)
+      .openLoopRampRate(0.25);
+
+    public static final double POSITION_CONVERSION_FACTOR = 1;
+    public static final double VELOCITY_CONVERSION_FACTOR = 1;
+    }
 }
--- src/main/java/frc/robot/subsystems/ArmRotatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ArmRotatorSubsystem.java
@@ -1,5 +1,78 @@
 package frc.robot.subsystems;
 
-public class ArmRotatorSubsystem {
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
+import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import static frc.robot.Constants.ArmRotatorConstants.*;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.FeedbackSensor;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class ArmRotatorSubsystem extends SubsystemBase {
     
+    SparkMax motor = new SparkMax(MOTOR_ID, MotorType.kBrushless);
+    RelativeEncoder motorEncoder = motor.getEncoder();
+
+    TrapezoidProfile trapProfile = new TrapezoidProfile(
+        new Constraints(MAX_VELOCITY, MAX_ACCELERATION)
+    );
+
+    State currentState = new State(motorEncoder.getPosition(),0);
+    State targetState = new State(motorEncoder.getPosition(),0);
+
+    SparkClosedLoopController closedLoopController = motor.getClosedLoopController();
+
+    public ArmRotatorSubsystem(){
+        MOTOR_CONFIG.encoder
+            .positionConversionFactor(POSITION_CONVERSION_FACTOR)
+            .velocityConversionFactor(VELOCITY_CONVERSION_FACTOR);
+        MOTOR_CONFIG
+            .closedLoop.feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+            .pid(P, I, D);
+        
+        motor.configure(MOTOR_CONFIG,ResetMode.kResetSafeParameters,PersistMode.kPersistParameters);
+        
+    }
+
+    @Override
+    public void periodic() {
+        targetState.position = MathUtil.clamp(
+            targetState.position,
+            MIN_ANGLE.getRotations(),
+            MAX_ANGLE.getRotations()
+        );   
+        currentState = trapProfile.calculate(1/50, currentState, targetState);
+        closedLoopController.setSetpoint(currentState.position, ControlType.kPosition);
+    }
+
+    public void setSetpoint(Rotation2d position){
+        targetState.position = position.getRotations();
+    }
+
+    public void incrementSetpoint(Rotation2d input){
+        targetState.position += input.getRotations();
+    }
+
+    public Rotation2d getAngle(){
+        return Rotation2d.fromRotations(motorEncoder.getPosition());
+    }
+
+    //#region Commands
+    public Command setSetpointCommand(Rotation2d setpoint){
+        return this.runOnce(()->setSetpoint(setpoint));
+    }
+
+    public Command incrementSetpointCommand(Rotation2d change){
+        return this.runOnce(()-> incrementSetpoint(change));
+    }
 }
